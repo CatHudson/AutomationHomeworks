3908f11e33477b079b857cd975cf51730cffe606
Arthur Cherkesov <arturhudson85@gmail.com>
2020-11-23 23:59:42 +0300

    Homework01

--- src/main/java/ru/geekbrains/Script02.java
+++ src/main/java/ru/geekbrains/Script02.java
@@ -20,9 +20,9 @@ public class Script02 {
 
         String temp = driver.findElement(By.xpath("//div[@class='temp fact__temp fact__temp_size_s']/span[@class='temp__value']")).getText();
 
-        System.out.println("Сейчас в Петербурге температура " + temp);
         Thread.sleep(5000);
         driver.quit();
+        System.out.println("Сейчас в Петербурге температура " + temp);
 
     }
 }
